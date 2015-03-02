3972cc9b47cad2c2574dd9afaf1b49a7d19fbe1e
Sandro <sandro.kuckert@safarisoft.de>
2015-03-02 14:58:07 +0100

    ng-route

--- app/app.js
+++ app/app.js
@@ -1,1 +1,22 @@
-var app = angular.module('lightSwitch', []);
+var app = angular.module('lightSwitch', ['ngRoute'])
+.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/Lamps/', {
+                templateUrl: 'app/lamps/lamp.view.html',
+                controller: 'LampController',
+                controllerAs: 'lamp',
+                resolve: {
+                    // I will cause a 1 second delay
+                    delay: function($q, $timeout) {
+                        var delay = $q.defer();
+                        $timeout(delay.resolve, 1000);
+                        return delay.promise;
+                    }
+                }
+            })
+            .when('/Lamps/:lampId/ch/:chapterId', {
+                templateUrl: 'chapter.html',
+                controller: 'ChapterController'
+            });
+    }]);
--- app/main.controller.js
+++ app/main.controller.js
@@ -1,6 +1,5 @@
-app.controller('MainController', ['$scope', function($scope) {
-    $scope.templates =
-        [ { name: 'template1.html', url: 'template1.html'},
-            { name: 'template2.html', url: 'template2.html'} ];
-    $scope.template = $scope.templates[0];
+app.controller('MainController', ['$route', '$routeParams', '$location', function($route, $routeParams, $location) {
+    this.$route = $route;
+    this.$location = $location;
+    this.$routeParams = $routeParams;
 }]);
